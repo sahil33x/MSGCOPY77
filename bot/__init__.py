c0350b0221f6d579437f4da1f444cb99fd392354
sahil33x <113387051+sahil33x@users.noreply.github.com>
2022-09-13 21:36:00 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,22 @@
+from pyrogram import Client
+from pyromod import listen
+
+api_id = 8763712
+api_hash = "835d27216f117e22a5c192b89a4ce457"
+bot_token = "5310301280:AAGLExVt5XtDNM-N-shGt_hCYrRdftAhyQg"
+tg_session = None
+sudo_users = [830785064, 872830003, 846709496, 927193767, 1542508017, 913846663, 983956963, 1700962147]
+
+
+if tg_session:
+  app = Client(tg_session, api_id, api_hash)
+elif bot_token:
+  app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+else:
+  print('Give Token Or Session!')
+  sys.exit(1)
+
+
+
+with app:
+  uname = app.get_me().username
